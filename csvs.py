b7cdc1b8201ad8cd04d26da73aa49dc1916b67e3
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-06 18:11:03 +0000

    Add files via upload

---
+++ csvs.py
@@ -0,0 +1,36 @@
+class csvss:
+  def __init__(self,value:str):
+    self.value=value
+    self.replaces='\n'
+    self.i=0
+    self.ii=False
+
+  def __next__(self)->str:
+
+    while(1):
+      if self.ii==True:
+        break
+      if self.i>=len(self.value):
+        break
+      a=self.value.find(self.replaces,self.i,len(self.value))
+      if a==-1:
+        t= self.value[self.i:len(self.value)]
+        self.i=len(self.value)
+        self.ii=True
+        return t.split (",")
+        break
+      else:
+        t= self.value[self.i:a]
+        self.i=a+len(self.replaces)
+        return t.split (",")
+
+    raise StopIteration
+
+  def __iter__(self):
+    return self
+
+print("\x1bc\x1b[43;30m")
+
+for a in csvss("hello,1,2,3\nworld,1,2,3\n10,1,2,3\n10,1,2,3"):
+    print(a)
+
