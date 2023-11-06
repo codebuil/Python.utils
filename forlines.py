41848308ca4075212014a0c041a2f4b81884fcd3
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-06 21:13:00 +0000

    Add files via upload

---
+++ forlines.py
@@ -0,0 +1,28 @@
+class openss:
+  def __init__(self,froms:str ):
+      self.into=open(froms,"r")
+      
+
+  def __next__(self):
+      try:
+        s=self.into.readline()
+        s=s.replace("\r","")
+        if s!="":
+          
+          return s
+        else:
+            self.into.close()
+            raise StopIteration
+      except:
+          self.into.close()
+          raise StopIteration      
+
+
+  def __iter__(self):
+      return self
+
+
+print("\x1bc\x1b[43;30m")
+
+for n in openss("my.txt"):
+  print(n)
