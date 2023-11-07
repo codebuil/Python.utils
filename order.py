b63869bbc1ff55076aa88067195f1e07fc650d1f
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-07 21:52:22 +0000

    Add files via upload

---
+++ order.py
@@ -0,0 +1,21 @@
+
+def listorder(l:list)->list:
+  l1=[]
+  i=0
+  for n in l:
+    l11=n
+    for m in range(i):
+      if l1[m]>l11:
+        l13=l1[m]
+        l1[m]=l11
+        l11=l13
+    
+    l1.append(l11)
+    i+=1
+  return l1
+
+print("\x1bc\x1b[43;30m")
+l1=["zaa","gz","az","bz","cz"]
+
+l2=listorder(l1)
+print (l2)
